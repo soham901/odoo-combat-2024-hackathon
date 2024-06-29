9914fc7bfe06a15ad679f62f0662c4c88305ddbe
Soham <sagathiyasoham12345@gmail.com>
2024-06-29 10:24:15 +0530

    layout updates

--- frontend/src/app/(site)/layout.tsx
+++ frontend/src/app/(site)/layout.tsx
@@ -6,9 +6,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex min-h-screen w-full flex-col">
+    <div className="flex min-h-screen w-full flex-col bg-muted/40">
       <Navbar />
-      <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10">
+      <main className="container mx-auto py-4 sm:py-6 flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4">
         {children}
       </main>
     </div>
